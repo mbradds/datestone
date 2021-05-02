218187ca7c637b04dfe8c7b7dda6db5861337541
grant <grantmossdev@gmail.com>
2021-05-02 11:16:48 -0600

    converted to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
-function changeValue(u) {
+interface Units {
+  conversion: number;
+  operation: string;
+  convert: boolean;
+}
+
+interface DateFunction {
+  (d: Date): number;
+}
+
+function changeValue(u: Units) {
   let valueTransformer;
-  if (u.base !== u.current) {
-    valueTransformer = function convert(row) {
+  if (u.convert) {
+    valueTransformer = function convert(row: number) {
       return row ? row * u.conversion : null;
     };
   } else {
-    valueTransformer = function dontConvert(row) {
+    valueTransformer = function dontConvert(row: number) {
       return row;
     };
   }
   return valueTransformer;
 }
 
-function addDate(f, i, outFormat) {
-  const dFunc = (d) => d.setDate(d.getDate() + i);
-  const mFunc = (d) => d.setMonth(d.getMonth() + i);
-  const yFunc = (d) => d.setFullYear(d.getFullYear() + i);
+function addDate(f: string, i: number, outFormat: string) {
+  const dFunc: DateFunction = (d) => d.setDate(d.getDate() + i);
+  const mFunc: DateFunction = (d) => d.setMonth(d.getMonth() + i);
+  const yFunc: DateFunction = (d) => d.setFullYear(d.getFullYear() + i);
 
-  let datePlusPlus;
+  let datePlusPlus: DateFunction = mFunc;
   if (f === "daily" || f === "d") {
     datePlusPlus = dFunc;
   } else if (f === "monthly" || f === "m") {
@@ -26,57 +36,54 @@ function addDate(f, i, outFormat) {
     datePlusPlus = yFunc;
   }
   if (outFormat === "date" || outFormat === "d") {
-    return function milliToDate(d) {
+    return function milliToDate(d: Date) {
       return new Date(datePlusPlus(d));
     };
   }
   return datePlusPlus;
 }
 
-function addRow(units, freq, increment, outFormat) {
+function addRow(
+  units: Units,
+  freq: string,
+  increment: number,
+  outFormat: string
+) {
   const dateFunction = addDate(freq, increment, outFormat);
   const rowFunction = changeValue(units);
-  return function adder(row, startDate) {
+  return function adder(row: number, startDate: Date) {
     const nextDate = dateFunction(startDate);
     return [nextDate, rowFunction(row)];
   };
 }
 
-export function applyDates({
-  series,
-  date,
-  transform = false,
+export function applyDates(
+  series: number[],
+  date: Date,
+  transform = { convert: false, operation: "none", conversion: 0 },
   frequency = "monthly",
   outputFormat = "number",
-  method = "forward",
-}) {
+  method = "forward"
+) {
   let increment = 1;
   if (method === "backward" || method === "b") {
     increment = -1;
   }
 
-  let startd;
-  if (typeof date !== "date") {
-    startd = new Date(date[0], date[1], date[2]);
-  } else {
-    startd = date;
-  }
-
   const addFunction = addRow(transform, frequency, increment, outputFormat);
   const nextSeries = series.map((row) => {
-    const [nextDate, rowCalc] = addFunction(row, startd);
+    const [nextDate, rowCalc] = addFunction(row, date);
     return [nextDate, rowCalc];
   });
 
   return nextSeries;
 }
 
-
 const testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const myDateRange = applyDates({
-  series: testData,
-  date: [2015, 1, 1],
-  //   outputFormat: "date",
-});
-console.log(myDateRange);
-module.exports = applyDates;
+const testDate = [2015, 1, 1];
+const myDateRange = applyDates(
+  testData,
+  new Date(testDate[0], testDate[1], testDate[2])
+);
+
+
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 import test from "ava";
-import { applyDates } from "./src/index.js";
-
-// const test = require("ava");
-// const applyDates = require("./src/index.js");
+import { applyDates } from "./dist/index.js";
 
 const testData1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const range1 = applyDates({ series: testData1, date: [2015, 1, 1] });
+const testDate1 = [2015, 1, 1];
+const range1 = applyDates(
+  testData1,
+  new Date(testDate1[0], testDate1[1], testDate1[2])
+);
 
 test("length", (t) => {
   t.is(testData1.length, range1.length);
